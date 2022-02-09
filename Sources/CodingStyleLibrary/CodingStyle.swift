1ed5a1a86b05678c0febd889f188905f4831d143
igor2890 <igor2890@mail.ru>
2022-02-09 21:50:02 +0300

    tested

--- Sources/CodingStyleLibrary/CodingStyle.swift
+++ Sources/CodingStyleLibrary/CodingStyle.swift
@@ -1,7 +1,11 @@
+import Foundation
+
 @propertyWrapper
 public struct CodingStyle {
     var value: String
     let codingStyle: CodingStyleEnum
+    
+//    private let symbols: [Character] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     
     public init(wrappedValue: String, with codingStyle: CodingStyleEnum) {
         self.value = wrappedValue
@@ -28,12 +32,45 @@ public struct CodingStyle {
     
     private func codingMethod() -> ((String) -> String){
         switch codingStyle {
-        case .camelCase:
-            return { $0 + "camelCase" }
-        case .snakeCase:
-            return { $0 + "snakeCase" }
-        case .kebabCase:
-            return { $0 + "kebabCase" }
+        case .camelCase: //need camelCase
+            return {
+                var splitedNew = $0.split(separator: "_")
+                var joinedNew = joinAndUppercasedFirstChars(subStrings: splitedNew)
+                
+                splitedNew = joinedNew.split(separator: "-")
+                joinedNew = joinAndUppercasedFirstChars(subStrings: splitedNew)
+                
+                return joinedNew
+            }
+        case .snakeCase: //need snake_case
+            return {
+                let new = $0.replacingOccurrences(of: "-", with: "_")
+                
+                let pattern = "([a-z0-9])([A-Z])"
+                let regex = try? NSRegularExpression(pattern: pattern, options: [])
+                let range = NSRange(location: 0, length: new.count)
+                return regex?.stringByReplacingMatches(in: new, options: [], range: range, withTemplate: "$1_$2").lowercased() ?? new
+            }
+            
+        case .kebabCase: //need kebab-case
+            return {
+                let new = $0.replacingOccurrences(of: "_", with: "-")
+                let pattern = "([a-z0-9])([A-Z])"
+                let regex = try? NSRegularExpression(pattern: pattern, options: [])
+                let range = NSRange(location: 0, length: new.count)
+                return regex?.stringByReplacingMatches(in: new, options: [], range: range, withTemplate: "$1-$2").lowercased() ?? new
+            }
         }
+    }
+    
+    private func joinAndUppercasedFirstChars(subStrings:[String.SubSequence]) -> String{
+        var strings = subStrings
+        if strings.count >= 2 {
+            for i in 1..<strings.count{
+                strings[i] = strings[i].prefix(1).uppercased() + strings[i].dropFirst()
+            }
+        }
+        let joinedString = strings.reduce("", +)
+        return joinedString
     }
 }
