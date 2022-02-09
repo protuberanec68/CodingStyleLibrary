d8b0bc035bcf72ce6b931b7655e370b28b58361e
igor2890 <igor2890@mail.ru>
2022-02-09 15:16:23 +0300

    some test prepares;2D

--- Sources/CodingStyleLibrary/CodingStyle.swift
+++ Sources/CodingStyleLibrary/CodingStyle.swift
@@ -1,9 +1,39 @@
-
-import Foundation
-
-public enum CodingStyle {
-    case camelCase
-    case snakeCase
-    case kebabCase
+@propertyWrapper
+public struct CodingStyle {
+    var value: String
+    let codingStyle: CodingStyleEnum
+    
+    public init(wrappedValue: String, with codingStyle: CodingStyleEnum) {
+        self.value = wrappedValue
+        self.codingStyle = codingStyle
+    }
+    
+    private func getValue() -> String{
+        let method = codingMethod()
+        return method(value)
+    }
+    
+    private mutating func setValue(newValue: String){
+        self.value = newValue
+    }
+    
+    public var wrappedValue: String {
+        get {
+            getValue()
+        }
+        set {
+            setValue(newValue: newValue)
+        }
+    }
+    
+    private func codingMethod() -> ((String) -> String){
+        switch codingStyle {
+        case .camelCase:
+            return { $0 + "camelCase" }
+        case .snakeCase:
+            return { $0 + "snakeCase" }
+        case .kebabCase:
+            return { $0 + "kebabCase" }
+        }
+    }
 }
-
---
+++ Sources/CodingStyleLibrary/CodingStyleEnum.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+
+public enum CodingStyleEnum {
+    case camelCase
+    case snakeCase
+    case kebabCase
+}
+
